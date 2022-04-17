5df7aa03cd2261d7aefa0270305d304651f328be
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2022-04-17 17:20:10 +0300

    test queen

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -108,3 +108,47 @@ describe Bishop do
     end
   end
 end
+
+describe Queen do
+  subject { Chess.new }
+  let(:board) { subject.rboard.board }
+  let(:queen) { board[7][3] }
+  let(:queen_coord) { subject.rboard.get_location(queen) }
+
+  context 'when moving' do
+    let(:new_coord) { [5, 3] }
+    let(:piece) { board[5][3] }
+
+    before do
+      allow(subject).to receive(:puts)
+      subject.rboard.basic_move(queen_coord, [5, 3])
+    end
+
+    it 'moves horizontal' do
+      subject.execute(new_coord, [5, 1], false, 'black', piece)
+      expect(board[5][1]).to eq(piece)
+    end
+
+    it 'moves vertical' do
+      subject.execute(new_coord, [3, 3], false, 'black', piece)
+      expect(board[3][3]).to eq(piece)
+    end
+
+    it 'moves diagonal' do
+      subject.execute(new_coord, [3, 5], false, 'black', piece)
+      expect(board[3][5]).to eq(piece)
+    end
+  end
+
+  context 'when facing other pieces' do
+    before do
+      allow(subject).to receive(:puts)
+    end
+
+    # fail
+    xit 'can\'t jump over other pieces' do
+      expect(subject).to receive(:puts).with('ILLEGAL MOVE'.red)
+      subject.execute(queen_coord, [5, 3], false, 'black', queen)
+    end
+  end
+end
